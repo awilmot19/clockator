6b9ee8e6b37d5cf20ab515345b7cf386e80f5476
sabrina-ren <ren.s.sabrina@gmail.com>
2013-11-09 04:59:22 -0500

    App basic structure

---
+++ clockator/ClockViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClockViewController.h
+//  clockator
+//
+//  Created by Sabrina Ren on 11/9/2013.
+//  Copyright (c) 2013 Clockator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClockViewController : UIViewController
+
+@end
---
+++ clockator/SettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.m
+//  clockator
+//
+//  Created by Sabrina Ren on 11/9/2013.
+//  Copyright (c) 2013 Clockator. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
