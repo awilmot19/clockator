51fbbb67d8e9418fb9ecd52d0f02a81647feb449
Andy Wilmot <awilmot19@gmail.com>
2013-11-08 23:25:43 -0500

    Initial Commit

---
+++ clockator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  clockator
+//
+//  Created by Andy Wilmot on 11/8/2013.
+//  Copyright (c) 2013 Clockator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
